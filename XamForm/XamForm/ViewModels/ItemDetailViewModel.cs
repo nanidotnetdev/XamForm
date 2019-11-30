820687b0497e1450bc3b558d96781157db2acb84
Narendra Nalluri <nanidotnetdev@gmail.com>
2019-11-30 17:11:16 -0600

    Initial commit

---
+++ XamForm/XamForm/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamForm.Models;
+
+namespace XamForm.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
